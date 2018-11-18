49475f87769a8454eac0031da8a22816e9299c14
Azmol-Miah <azmol@azmolmiah.com>
2018-11-18 11:55:37 +0000

    edit applied

--- app.js
+++ app.js
@@ -84,6 +84,34 @@ app.post("/articles/add", (req, res) => {
   });
 });
 
+// Load edit form
+app.get("/article/edit/:id", (req, res) => {
+  Article.findById(req.params.id, (err, article) => {
+    res.render("edit_article", {
+      title: "Edit Article",
+      article: article
+    });
+  });
+});
+
+// Add submit POST route
+app.post("/articles/edit/:id", (req, res) => {
+  let article = [];
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = { _id: req.params.id };
+  Article.update(query, article, err => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
 //start server
 app.listen(3000, () => {
   console.log("Server started on port 3000");
