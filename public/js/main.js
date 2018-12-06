7407c791c0d4014bcb8ca59c316e588698c4ca30
Azmol-Miah <azmol@azmolmiah.com>
2018-12-06 10:30:55 +0000

    So now the alarm time is working just need to get setTimeout to work

--- public/js/main.js
+++ public/js/main.js
@@ -32,43 +32,59 @@ $(document).ready(() => {
   });
 });
 
-// GET the time and date then alert
-const dates = document.querySelectorAll("p.date");
-const times = document.querySelectorAll("p.time");
-const today = new Date();
+// // GET the time and date then alert
+function renderTime() {
+  const dates = document.querySelectorAll("p.date");
+  const times = document.querySelectorAll("p.time");
+  const today = new Date();
 
-// Date
-let date = today.getDate();
-let month = today.getMonth() + 1;
-let year = today.getFullYear();
-let todaysDate;
+  // Date
+  let date = today.getDate();
+  let month = today.getMonth() + 1;
+  let year = today.getFullYear();
+  let todaysDate;
 
-date < 10
-  ? (todaysDate = "0" + date + "/" + month + "/" + year)
-  : (todaysDate = date + "/" + month + "/" + year);
+  date < 10
+    ? (todaysDate = "0" + date + "/" + month + "/" + year)
+    : (todaysDate = date + "/" + month + "/" + year);
 
-let myDates;
-dates.forEach(date => {
-  myDates = date.innerHTML
-    .split("-")
-    .reverse()
-    .join("/");
-  date.innerHTML = myDates;
-});
+  let myDates;
+  dates.forEach(date => {
+    myDates = date.innerHTML
+      .split("-")
+      .reverse()
+      .join("/");
+    date.innerHTML = myDates;
+  });
 
-// Time
-let hourNow = today.getHours();
-let minutesNow = today.getMinutes();
-let currentTime = hourNow + ":" + minutesNow;
-let myCurrentTime;
+  let dayNumber;
+  let monthNumber;
+  let yearNumber;
 
-times.forEach(time => {
-  let splitTime = time.innerHTML.split(":");
-  let hourNumber = Number(splitTime[0]);
-  let minutesNumber = Number(splitTime[1]);
-  myCurrentTime = hourNumber + ":" + minutesNumber;
-});
+  dates.forEach(date => {
+    let splitDate = date.innerHTML.split("/");
+    dayNumber = Number(splitDate[0]);
+    monthNumber = Number(splitDate[1]);
+    yearNumber = Number(splitDate[2]);
+  });
 
-if (currentTime == myCurrentTime && todaysDate == myDates) {
-  console.log("Hello");
+  // Time
+  let hourNow = today.getHours();
+  let minutesNow = today.getMinutes();
+  let hourNumber;
+  let minutesNumber;
+
+  times.forEach(time => {
+    let splitTime = time.innerHTML.split(":");
+    hourNumber = Number(splitTime[0]);
+    minutesNumber = Number(splitTime[1]);
+  });
+  if (dayNumber == date && monthNumber == month && yearNumber == year) {
+    if (hourNumber == hourNow && minutesNumber == minutesNow) {
+      clearTimeout("renderTime()");
+      console.log("Hello");
+    }
+  }
+  setTimeout("renderTime()", 1000);
 }
+renderTime();
